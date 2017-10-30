58cfaba0990e7676c878a8237ed43d8647905930
Ketan Kulkarni <ketkulka@gmail.com>
2017-10-30 13:53:44 -0700

    fixed more variables and introduced gitignore

--- .fzf.bash
+++ .fzf.bash
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */users/ketkulka/.fzf/bin* ]]; then
-  export PATH="$PATH:/users/ketkulka/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/users/ketkulka/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/users/ketkulka/.fzf/shell/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
 
